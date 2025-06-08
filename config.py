e0f737e914ef2324f34195610aa8370a11a7fad7
dk8827 <121302433+dk8827@users.noreply.github.com>
2025-06-08 23:33:25 +0300

    changed constants

--- config.py
+++ config.py
@@ -57,22 +57,22 @@ DQN_CONFIG = {
     'GAMMA': 0.99,       # Discount factor
     'LR': 1e-4,          # Learning Rate
     'TAU': 0.005,        # Target network soft update rate
-    'MEMORY_CAPACITY': 50000,
+    'MEMORY_CAPACITY': 100000,
     'UPDATE_EVERY': 4,              # How often to run the optimization step
-    'TARGET_UPDATE_EVERY': 1000,     # How often to soft-update the target network
+    'TARGET_UPDATE_EVERY': 20,     # How often to soft-update the target network
     'CHECKPOINT_EVERY': 50000,
-    'GRAD_CLIP': 100,
+    'GRAD_CLIP': 10,
     # Parameter Space Noise
     'NOISE_ENABLED': True,
     'NOISE_SCALE_START': 0.05,
     'NOISE_SCALE_END': 0.01,
     'NOISE_SCALE_DECAY': 100000,
     # Rewards
-    'REWARD_GOAL': 1,
-    'REWARD_BALL_PROXIMITY_SCALE': 1,   # Scales reward for being close to the ball (inversely proportional to distance)
-    'REWARD_FACING_BALL_SCALE': 0.3,    # Scales reward for facing the ball
-    'PENALTY_CONCEDE': -0.8,
-    'PENALTY_STATIONARY': -1,           # Penalty for not moving over a window of steps
+    'REWARD_GOAL': 10,
+    'REWARD_BALL_PROXIMITY_SCALE': 0.05,   # Scales reward for being close to the ball (inversely proportional to distance)
+    'REWARD_FACING_BALL_SCALE': 0.1,    # Scales reward for facing the ball
+    'PENALTY_CONCEDE': -10,
+    'PENALTY_STATIONARY': -0.1,           # Penalty for not moving over a window of steps
     'STATIONARY_WINDOW': 200,             # Number of steps to check for inactivity
     'STATIONARY_THRESHOLD': 2.0,          # Max distance moved in the window to be considered stationary
 }
